caff06137f6ec275f22cfdcef1b5d516d6ae2e44
Meagan Carlesso <meaganc@au1.ibm.com>
2017-09-30 22:19:59 +1000

    Started adding layout and styling to view ticket page

--- admin/src/components/Ticket.js
+++ admin/src/components/Ticket.js
@@ -1,15 +1,67 @@
 import React from 'react'
+import {Card, CardActions, CardHeader, CardText, TextField} from "material-ui";
 
-export default class Ticket {
+export default class Ticket extends React.Component {
     componentWillMount()
     {
         if (!localStorage.getItem('User'))
         {
             this.props.history.push("/login");
-            return;
+            return ;
         }
     }
     render() {
-
+            return <TicketCard/>
     }
 }
+
+const styles = {
+    leftCard: {
+        float: 'left'
+    },
+    rightCard: {
+        float: 'right'
+    }
+}
+
+const TicketCard = () => (
+<div>
+    <Card style={styles.leftCard}>
+        <CardHeader>
+            Ticket
+        </CardHeader>
+        <CardText>
+            <TextField
+                hintText="Hint Text"
+                floatingLabelText="Floating Label Text"
+            />
+            <br/>
+            <TextField
+                hintText="Hint Text"
+                floatingLabelText="Floating Label Text"
+            />
+            <br/>
+            <TextField
+                hintText="Hint Text"
+                floatingLabelText="Floating Label Text"
+            />
+            <br/>
+            
+        </CardText>
+        <CardActions>
+
+        </CardActions>
+    </Card>
+    <Card style={styles.rightCard}>
+        <CardHeader>
+            Ticket Comments
+        </CardHeader>
+        <CardText>
+
+        </CardText>
+        <CardActions>
+
+        </CardActions>
+    </Card>
+</div>
+);
